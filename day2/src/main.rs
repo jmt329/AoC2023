5e8282b7b32f2c3add77b5d70adb02034f366a1f
James <jmt329@cornell.edu>
2023-12-02 15:26:34 -0500

    add day2 part 2 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Cubes {
     red : u32,
     green : u32,
@@ -26,6 +26,18 @@ impl Cubes {
     fn possible_bag(&self, other: &Cubes) -> bool {
         (self.red <= other.red) && (self.green <= other.green) && (self.blue <= other.blue)
     }
+
+    fn min_bag(lhs: &Cubes, rhs: &Cubes) -> Self {
+        Self{
+            red: std::cmp::max(lhs.red, rhs.red),
+            green: std::cmp::max(lhs.green, rhs.green),
+            blue: std::cmp::max(lhs.blue, rhs.blue)
+        }
+    }
+
+    fn power(&self) -> u32 {
+        self.red * self.green * self.blue
+    }
 }
 
 fn parse_line(draws: &str) -> Vec<Cubes> {
@@ -40,15 +52,17 @@ fn main() {
     let file_lines = file_string.lines();
 
     let mut id_acc: u32 = 0; // accumulator for ids for games that are possible
+    let mut power_acc = 0;
     for line in file_lines {
         // parse each game
         let mut l = line.split(": ");
         let (game_info, draws) = (l.next().unwrap(), l.next().unwrap());
         let num : u32 = game_info.matches(char::is_numeric).collect::<String>().parse().unwrap();
         let game = parse_line(draws);
-        let possible = game.iter().map(|h| h.possible_bag(&Cubes{red: 12, green: 13, blue: 14})).reduce(|acc, e| acc && e).unwrap();
-        //println!("Game num: {}, cubes: {:?}, possible: {}", num, game, possible);
+        let possible = game.clone().iter().map(|h| h.possible_bag(&Cubes{red: 12, green: 13, blue: 14})).reduce(|acc, e| acc && e).unwrap();
         if possible {id_acc += num;}
+        power_acc += game.iter().fold(Cubes{red: 0, green:0, blue: 0}, |acc, e| Cubes::min_bag(&acc, e)).power();
     }
-    println!("p1: {}", id_acc)
+    println!("p1: {}", id_acc);
+    println!("p2: {}", power_acc);
 }
