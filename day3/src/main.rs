eb6c1b08f4ba0ff0baad2b4c70c72de4da622894
James <jmt329@cornell.edu>
2023-12-04 21:36:07 -0500

    add day 3 part 2 solution

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,10 +1,28 @@
-use std::fs;
+use std::{fs, ops};
 
 #[derive(Debug)]
 struct NumPos {
     num: u32,
     idxs: Vec<usize>,
-    part: bool
+    part: bool,
+}
+
+struct SymbolPos<'a> {
+    idx: usize,
+    symbol: &'a str,
+}
+
+#[derive(Debug)]
+struct Gear {
+    num: usize,
+    ratio: u32,
+}
+
+impl ops::AddAssign for Gear {
+    fn add_assign(&mut self, rhs: Self) {
+        self.num += rhs.num;
+        self.ratio *= rhs.ratio;
+    }
 }
 
 fn get_numbers(s: &str) -> Vec<NumPos> {
@@ -30,7 +48,7 @@ fn get_numbers(s: &str) -> Vec<NumPos> {
                 ret.push(NumPos {
                     num: acc,
                     idxs: idx_vec,
-                    part: false
+                    part: false,
                 });
             }
             acc = num;
@@ -42,22 +60,46 @@ fn get_numbers(s: &str) -> Vec<NumPos> {
         ret.push(NumPos {
             num: acc,
             idxs: idx_vec,
-            part: false
+            part: false,
         });
     }
     ret
 }
 
-fn get_symbol_positions(s: &str) -> Vec<usize> {
-    s.match_indices(|x: char| !x.is_numeric() && (x != '.')).map(|(i, _)| i).collect()
+fn get_symbol_positions(s: &str) -> Vec<SymbolPos> {
+    s.match_indices(|x: char| !x.is_numeric() && (x != '.'))
+        .map(|(i, e)| SymbolPos { idx: i, symbol: e })
+        .collect()
 }
 
-fn set_part(nps : &mut Vec<NumPos>, idx : usize) {
+fn set_part(nps: &mut Vec<NumPos>, idx: usize) {
     for n in nps {
-        if n.idxs.contains(&(idx - 1)) {n.part = true};
-        if n.idxs.contains(&idx) {n.part = true};
-        if n.idxs.contains(&(idx + 1)) {n.part = true};
+        if n.idxs.contains(&(idx - 1)) {
+            n.part = true;
+        };
+        if n.idxs.contains(&idx) {
+            n.part = true
+        };
+        if n.idxs.contains(&(idx + 1)) {
+            n.part = true
+        };
     }
+}
+
+fn get_gear(nps: &Vec<NumPos>, sym: &SymbolPos) -> Gear {
+    let mut ret = Gear { num: 0, ratio: 1 };
+    if sym.symbol.contains('*') {
+        for n in nps {
+            if n.idxs.contains(&(sym.idx - 1))
+                || n.idxs.contains(&sym.idx)
+                || n.idxs.contains(&(sym.idx + 1))
+            {
+                ret.num += 1;
+                ret.ratio *= n.num;
+            }
+        }
+    }
+    ret
 }
 
 fn main() {
@@ -65,6 +107,7 @@ fn main() {
     let file_lines = file_string.lines();
     let mut numbers = vec![];
     let mut sympos = vec![];
+    let mut gears = vec![];
 
     for line in file_lines {
         numbers.push(get_numbers(line));
@@ -74,19 +117,34 @@ fn main() {
     // find part numbers
     for (line_num, sym) in sympos.iter().enumerate() {
         for s in sym {
+            let mut g = Gear { num: 0, ratio: 1 };
             if line_num > 0 {
                 // look at line above
-                set_part(&mut numbers[line_num - 1], *s);
+                set_part(&mut numbers[line_num - 1], s.idx);
+                g += get_gear(&numbers[line_num - 1], s);
             }
             if line_num < (numbers.len() - 1) {
                 // look at line below
-                set_part(&mut numbers[line_num + 1], *s);
+                set_part(&mut numbers[line_num + 1], s.idx);
+                g += get_gear(&numbers[line_num + 1], s);
             }
             // look at current line
-            set_part(&mut numbers[line_num], *s);
+            set_part(&mut numbers[line_num], s.idx);
+            g += get_gear(&numbers[line_num], s);
+            gears.push(g);
         }
     }
 
-    let p1 = numbers.iter().flatten().filter(|e| e.part).fold(0, |e, acc| e+acc.num);
-    println!("p1: {}", p1)
+    let p1 = numbers
+        .iter()
+        .flatten()
+        .filter(|e| e.part)
+        .fold(0, |acc: u32, e| acc + e.num);
+    println!("p1: {}", p1);
+
+    let p2 = gears
+        .iter()
+        .filter(|e| e.num == 2)
+        .fold(0, |acc, e| acc + e.ratio);
+    println!("p2 {}", p2);
 }
